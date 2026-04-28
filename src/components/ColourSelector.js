18aaa0215c6b92e80f327101cc70550832f40f7d
imhsart <nitinrana2519@gmail.com>
2026-04-28 18:49:30 +0530

    Assignment attempt 3

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
-  const { background, label } = config;
+  const { background, label, key } = config;
   return (
     <button style={{backgroundColor: `${background}`}} 
-    onClick={() => selectNextBackground({background: background})}>
+    onClick={() => selectNextBackground({background: background,key:key})}>
       {label}
     </button>
   )
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,10 +3,13 @@ import '../styles/Child.css';
 
 const Selection = ({applyColor}) => {
   const [bgColor, setBgColor] = useState('')
-
+  const [testId, setTestId] = useState('')
   return (
-    <div className="fix-box" data-testid={bgColor} style={{backgroundColor: `${bgColor}`}} onClick={() => {
-      applyColor((color) => setBgColor(color.background))
+    <div className="fix-box" data-testid={testId} style={{backgroundColor: `${bgColor}`}} onClick={() => {
+      applyColor((color) => {
+        setBgColor(color.background)
+        setTestId(color.key)
+      })
       }}>
       <h2 className="subheading">Selection</h2>
     </div>

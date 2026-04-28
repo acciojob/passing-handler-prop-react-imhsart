73f0360d8e15f0200ca4563b164d2f86c5417885
imhsart <nitinrana2519@gmail.com>
2026-04-28 18:08:58 +0530

    Assignment attempt 1

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, {useState} from "react";
+import '../styles/Child.css';
 
+const Selection = ({applyColor}) => {
+  const [bgColor, setBgColor] = useState('')
+
+  return (
+    <div className="fix-box" style={{backgroundColor: `${bgColor}`}} onClick={() => {
+      applyColor((color) => setBgColor(color.background))
+      }}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
+
+
+export default Selection
